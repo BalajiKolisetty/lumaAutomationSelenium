879b936368fdc141a0c1d7bbbb1e24f9a10ad9fc
Balaji Kolisetty <balaji.kolisetty@gmail.com>
2023-05-12 16:56:14 +0530

    Added login method covering the minimum validations

--- src/main/java/com/luma/qa/pages/LoginPage.java
+++ src/main/java/com/luma/qa/pages/LoginPage.java
@@ -1,6 +1,9 @@
 package com.luma.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 import com.luma.qa.base.TestBase;
 
@@ -10,6 +13,51 @@ public class LoginPage extends TestBase{
 		PageFactory.initElements(driver, this);
 	}
 	
+	@FindBy(xpath="//input[@id='email']")
+	WebElement emailIdField;
 	
-
+	@FindBy(xpath="//input[@id='pass']")
+	WebElement passwordField;
+	
+	@FindBy(xpath="//button[@id='send2']")
+	WebElement loginButton;
+	
+	@FindBy(linkText="Forgot Your Passowrd?")
+	WebElement passwordResetLink;
+	
+	@FindBy(xpath="//div[@id='email-error']")
+	WebElement emailFieldError;
+	
+	@FindBy(xpath="//div[@id='pass-error']")
+	WebElement passwordFieldError;
+	
+	@FindBy(xpath="//*[@id=\"maincontent\"]/div[2]/div[2]/div/div/div")
+	WebElement invalidCredentialsOrSomeError;
+	
+	public void login(String email, String password) {
+		try {			
+			emailIdField.sendKeys(email);
+			passwordField.sendKeys(password);
+			loginButton.click();
+		}
+		
+		catch (Exception e) {			
+			if (emailIdField.getAttribute("value").isEmpty() && passwordField.getAttribute("value").isEmpty()) {
+				Assert.assertTrue(emailFieldError.isDisplayed());
+				Assert.assertTrue(passwordFieldError.isDisplayed());
+			}
+			
+			else if (emailIdField.getAttribute("value").isEmpty()) {
+				Assert.assertTrue(emailFieldError.isDisplayed());
+			}
+			
+			else if (passwordField.getAttribute("value").isEmpty()) {
+				Assert.assertTrue(passwordFieldError.isDisplayed());
+			}
+			
+			else {
+				Assert.assertTrue(invalidCredentialsOrSomeError.isDisplayed());
+			}
+		}	
+	}
 }
