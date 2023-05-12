a2cdbf462502992272830efb857b84cf7d0f2be8
Balaji Kolisetty <balaji.kolisetty@gmail.com>
2023-05-12 16:54:20 +0530

    Added methods validating things on miniCartIcon and productSearchButton

--- src/main/java/com/luma/qa/pages/CommonHeaderLinksSection.java
+++ src/main/java/com/luma/qa/pages/CommonHeaderLinksSection.java
@@ -1,8 +1,14 @@
 package com.luma.qa.pages;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import com.luma.qa.base.TestBase;
 
@@ -27,6 +33,15 @@ public class CommonHeaderLinksSection extends TestBase {
 	@FindBy(xpath="//a[@class='action showcart']")
 	WebElement miniCartIcon;
 	
+	@FindBy(xpath="//span[@class='counter qty']")
+	WebElement cartWhenQuantityIsPresent;
+	
+	@FindBy(xpath="//strong[contains(., 'You have no items in your shopping cart.')]")
+	WebElement noProductsInCartMessage;
+	
+	@FindBy(xpath="//button[@id='top-cart-btn-checkout']")
+	WebElement checkoutButtonDisplayInMiniCart;
+	
 	@FindBy(xpath="//span[contains(., 'Women')]")
 	WebElement womenProductsDropdown;
 	
@@ -48,4 +63,31 @@ public class CommonHeaderLinksSection extends TestBase {
 		createNewAccountLink.click();
 		return new RegistrationPage();
 	}
+	
+	public void miniCartIconBasicValidation() {
+		String stringQuantityInCart = cartWhenQuantityIsPresent.getText();
+		
+		try {
+			int numberQuantityInCart = Integer.parseInt(stringQuantityInCart); 
+			if (numberQuantityInCart > 0) {
+				WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+				miniCartIcon.click();
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='top-cart-btn-checkout']")));
+				Assert.assertTrue(checkoutButtonDisplayInMiniCart.isDisplayed());
+			}
+		}
+		
+		catch (Exception e) {
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			miniCartIcon.click();
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//strong[contains(., 'You have no items in your shopping cart.')]")));
+			Assert.assertTrue(noProductsInCartMessage.isDisplayed());
+		}
+	}
+	
+	public void productSearchButtonIsDisabled() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+		Assert.assertFalse(productSearchButton.isEnabled());		
+	}
 }
+
