227443ced24b50e47a2047481a9e821d5c5ffa69
Balaji Kolisetty <balaji.kolisetty@gmail.com>
2023-05-24 19:16:09 +0530

    Adding cart page to the repo

---
+++ src/main/java/com/luma/qa/pages/CartPage.java
@@ -0,0 +1,23 @@
+package com.luma.qa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import com.luma.qa.base.TestBase;
+
+public class CartPage extends TestBase {
+	
+	public CartPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//span[contains(., 'Shopping Cart')]")
+	WebElement cartPageHeading;
+	
+	public void cartPageHeadingDisplay() {
+		Assert.assertTrue(cartPageHeading.isDisplayed());
+	}
+
+}
